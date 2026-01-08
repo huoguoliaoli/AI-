5a96a0578619aa7ea0603d31c8326646d594d9e7
huoguoliaoli <66119517+huoguoliaoli@users.noreply.github.com>
2026-01-08 14:45:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function sayHello() {
-    alert("欢迎来到我的网站！");
-  }
-  
+const list = document.getElementById("list");
+
+aiSites.forEach(site => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+    <img src="${site.cover}" />
+    <div class="content">
+      <h3>${site.name}</h3>
+      <p>${site.desc}</p>
+    </div>
+  `;
+
+  card.onclick = () => {
+    window.location.href = `detail.html?id=${site.id}`;
+  };
+
+  list.appendChild(card);
+});
